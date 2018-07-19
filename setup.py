af5e54427df400c748584f8aa319777ac621566c
dgaiku007 <dgaiku007@users.noreply.github.com>
2018-07-19 09:05:43 +0300

    1st commit

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup
 setup(name='ie_mbdbl2018_dmburu',
       version='0.1',  # Development release
       description='Python Packaging Starting Kit - Simple Structure to creater your own package - for training purposes!',
-      url='https://github.com/dgaiku007',
+      url='https://github.com/dgaiku007/ie_mbdbl2018_dmburu.git',
       author='DAN GAIKU MBURU',
       author_email='dan.mburu@student.ie.edu',
       license='MIT',
           packages=['ie_mbdbl2018_dmburu'],
       zip_safe=False)
+	  
