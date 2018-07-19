6ea9ac6236ecd79adca2c7f9ee86c05f48eb4d74
dgaiku007 <dgaiku007@users.noreply.github.com>
2018-07-19 08:33:21 +0300

    1st commit

---
+++ ie_mbdbl2018_dmburu/__init__.py
@@ -0,0 +1,3 @@
+def  datareader_doc():
+    """ function to return the string 'My first Python package that can be installed by anyone! How cool!!!' """
+    return(""This is DAN GAIKU MBURU's first package that can be installed by anyone! How cool!!!"")
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='pythonpackagesample',
+setup(name='ie_mbdbl2018_dmburu',
       version='0.1',  # Development release
       description='Python Packaging Starting Kit - Simple Structure to creater your own package - for training purposes!',
       url='https://github.com/manoelgadi/pythonpackagesample',
-      author='Prof. Manoel Gadi',
-      author_email='mfalonso@faculty.ie.edu',
+      author='Dan Gaiku Mburu',
+      author_email='dan.mburu@student.ie.edu',
       license='MIT',
-          packages=['pythonpackagesample'],
+          packages=['ie_mbdbl2018_dmburu'],
       zip_safe=False)
